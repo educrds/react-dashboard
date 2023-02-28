664ab9359dd0a5ea8fa4aeab0fb35798c2e14805
Eduardo Cardoso <eduardocardoso1039@gmail.com>
2023-02-28 18:39:37 -0300

    Created Email Input Component

---
+++ src/components/EmailInput/index.tsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import { AiOutlineEye, AiOutlineEyeInvisible } from 'react-icons/ai';
+import './styles.scss';
+
+const PasswordInput = () => {
+  const [email, setEmail] = useState('');
+
+  const handleEmailChange = event => {
+    setEmail(event.target.value);
+  };
+
+  return (
+    <div className='form__group field'>
+      <input
+        type='email'
+        className='form__field'
+        value={email}
+        onChange={handleEmailChange}
+        placeholder='Email'
+        name='Email'
+        id='Email'
+        required
+      />
+      <label htmlFor='Email' className='form__label'>
+        Email
+      </label>
+    </div>
+  );
+};
+
+export default PasswordInput;
