169f68b3b93bcccccb21422dbcf930f0d2ad6ed5
Eduardo Cardoso <eduardocardoso1039@gmail.com>
2023-02-28 16:01:11 -0300

    Created SignIn/Up Google Tag

---
+++ src/components/GoogleTag/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './styles.scss';
+import { FcGoogle } from 'react-icons/fc';
+
+type Props = {
+  text: string;
+};
+
+const GoogleTag = ({ text }: Props) => (
+  <div className='google-tag'>
+    <div className='google-icon'>
+      <FcGoogle />
+    </div>
+    <div className='text'>{text}</div>
+  </div>
+);
+
+export default GoogleTag;
