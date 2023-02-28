c2a32efb57a2b152508aac5cff891b07820e90ce
Eduardo Cardoso <eduardocardoso1039@gmail.com>
2023-02-28 16:01:59 -0300

    Created Login Page Component

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,8 +1,18 @@
 import React from 'react';
+import Brand from '../../components/Brand';
+import GoogleTag from '../../components/GoogleTag';
 import './styles.scss';
 
 const Login = () => {
-  return <h1>Login</h1>;
+  return (
+    <div className='container'>
+      <div className='brand'>
+        <Brand />
+        <h3>Sua nova ferramenta para alcançar suas metas financeiras!</h3>
+      </div>
+      <GoogleTag text='Entrar com Google' />
+    </div>
+  );
 };
 
 export default Login;
