a8993a1145aa607a90efdd8cc46fd55f425a7c61
Eduardo Cardoso <eduardocardoso1039@gmail.com>
2023-02-28 16:01:46 -0300

    Created Brand Container Component

---
+++ src/components/Brand/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import logo from '../../assets/imgs/Coinz.png';
+import './styles.scss';
+
+const Brand = () => <img className='logo' src={logo} alt='coinz logo' />;
+
+export default Brand;
