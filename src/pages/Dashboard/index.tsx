013d0eb54839c57ff1b423eb87f6e3b77130d545
Eduardo Cardoso <eduardocardoso1039@gmail.com>
2023-03-02 15:50:03 -0300

    Created Dashboard Component

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,8 +1,26 @@
 import React from 'react';
-import Navbar from '../../components/Navbar';
+import './styles.scss';
+import DashboardOverview from '../../components/DashboardOverview';
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return (
+    <div className='dashboard__container'>
+      <MenuBar />
+      <DashboardOverview />
+    </div>
+  );
+};
+
+const MenuBar = () => {
+  return (
+    <div className='dashboard__bar'>
+      <h1>Dashboard</h1>
+      <img
+        src='https://www.pngall.com/wp-content/uploads/5/Profile-Avatar-PNG.png'
+        alt='profile photo'
+      />
+    </div>
+  );
 };
 
 export default Dashboard;
