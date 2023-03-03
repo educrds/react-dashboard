2b8e56e51ae0f256a85d10b46ef0976eabc8fc98
Eduardo Cardoso <eduardocardoso1039@gmail.com>
2023-03-03 17:54:06 -0300

    Created Doughnut Charts

--- src/components/DashboardChartBar/index.tsx
+++ src/components/DashboardChartBar/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Bar } from 'react-chartjs-2';
-import { options, data } from '../../charts/chartConfig';
+import { options, data } from '../../charts/barChartConfig';
 import DashboardItem from '../DashboardItem';
 import './styles.scss';
 
