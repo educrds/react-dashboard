377895b4ddc407519a2818351cb7433064403d6a
Eduardo Cardoso <eduardocardoso1039@gmail.com>
2023-03-06 17:14:31 -0300

    refactor(Dashboard Square Overview Style): Refactored Dashboard Square Overview Style

--- src/components/DashboardOverview/index.tsx
+++ src/components/DashboardOverview/index.tsx
@@ -1,33 +1,66 @@
+import {
+  AccountBalanceOutlined,
+  TrendingDownRounded,
+  TrendingUpRounded,
+  KeyboardArrowRightRounded,
+} from '@mui/icons-material';
+import { Link } from 'react-router-dom';
+import React, { ElementType } from 'react';
 import DashboardItem from '../DashboardItem';
 import './styles.scss';
 
-const data = [
-  { title: 'Receitas', value: 'R$5.000', color: 'circle__green' },
-  { title: 'Balanço', value: 'R$2.000', color: 'circle__blue' },
-  { title: 'Despesas', value: 'R$3.000', color: 'circle__red' },
+interface DataItem {
+  title: string;
+  value: string;
+  color: string;
+  icon: ElementType;
+  to?: string;
+}
+
+const data: DataItem[] = [
+  {
+    title: 'Receitas',
+    value: 'R$5.000',
+    color: 'icon__green',
+    icon: <TrendingUpRounded />,
+    to: '/receitas',
+  },
+  { title: 'Balanço', value: 'R$2.000', color: 'icon__grey', icon: <AccountBalanceOutlined /> },
+  {
+    title: 'Despesas',
+    value: 'R$3.000',
+    color: 'icon__red',
+    icon: <TrendingDownRounded />,
+    to: '/despesas',
+  },
 ];
 
 const DashboardOverview = () => {
   return (
     <DashboardItem>
-      {data.map((square, i) => <DashboardItemSquare key={i} {...square} /> )}
+      {data.map((square, i) => (
+        <DashboardItemSquare key={i} {...square} />
+      ))}
     </DashboardItem>
   );
 };
 
-interface DashboardItemSquareProps {
-  title: string;
-  value: string;
-  color: string;
-}
+interface DashboardItemSquareProps extends DataItem {}
 
-const DashboardItemSquare = ({ title, value, color }: DashboardItemSquareProps) => {
+const DashboardItemSquare = ({ title, value, color, icon, to }: DashboardItemSquareProps) => {
   return (
     <div className='dashboard__item__square'>
       <div>
         <div className='item__square__title'>
-          <div className={`item__square__circle ${color}`}></div>
-          <span>{title}</span>
+          <div className={`item__square__title ${color}`}>{icon}</div>
+          {to ? (
+            <Link to={to}>
+              <span>{title}</span>
+              <KeyboardArrowRightRounded />
+            </Link>
+          ) : (
+            <span>{title}</span>
+          )}
         </div>
         <div className='item__square__value'>{value}</div>
       </div>
