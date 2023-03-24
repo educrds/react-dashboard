b791b66f6b2f6f02e45ae31bef9e564d55e308b7
Eduardo <eduardocardoso1039@gmail.com>
2023-03-24 18:04:31 -0300

    build(Added Redux to Components): Added Redux to Components & Setup Data

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes, Outlet } from 'react-router-dom';
-import './styles/css/style.css';
 import Dashboard from './pages/Dashboard';
 import Revenue from './pages/Revenue';
 import Expenses from './pages/Expense';
@@ -9,30 +8,35 @@ import Navbar from './components/Navbar';
 import Login from './pages/Auth/Login';
 import Register from './pages/Auth/Register';
 import { NavbarContextProvider } from './contexts/NavbarContext';
+import { Provider } from 'react-redux';
+import { store } from './services/transactions';
+import './styles/css/style.css';
 
 const App: React.FC = () => {
   return (
     <NavbarContextProvider>
       <BrowserRouter>
-        <Routes>
-          <Route element={<Outlet />}>
-            <Route path='/' element={<Login />} />
-            <Route path='/register' element={<Register />} />
-            <Route
-              element={
-                <>
-                  <Navbar />
-                  <Outlet />
-                </>
-              }
-            >
-              <Route path='/dashboard' element={<Dashboard />} />
-              <Route path='/receitas' element={<Revenue />} />
-              <Route path='/despesas' element={<Expenses />} />
-              <Route path='/transacoes' element={<Transactions />} />
+        <Provider store={store}>
+          <Routes>
+            <Route element={<Outlet />}>
+              <Route path='/' element={<Login />} />
+              <Route path='/register' element={<Register />} />
+              <Route
+                element={
+                  <>
+                    <Navbar />
+                    <Outlet />
+                  </>
+                }
+              >
+                <Route path='/dashboard' element={<Dashboard />} />
+                <Route path='/receitas' element={<Revenue />} />
+                <Route path='/despesas' element={<Expenses />} />
+                <Route path='/transacoes' element={<Transactions />} />
+              </Route>
             </Route>
-          </Route>
-        </Routes>
+          </Routes>
+        </Provider>
       </BrowserRouter>
     </NavbarContextProvider>
   );
